163e3fb70dc1abb1b843d1e4b1997e2062822822
lazymaster101 <136504717+lazymaster101@users.noreply.github.com>
2023-06-13 22:06:38 +0000

    Implement rightRotation

--- src/App.java
+++ src/App.java
@@ -1,9 +1,25 @@
 import java.util.Arrays;
 
+import javax.swing.text.DefaultStyledDocument.ElementSpec;
+
 public class App {
   
   public static Object[] rotateRight(Object[] arr) {
-	  return arr;
+	  Object[] newarr=new Object[arr.length];
+    for(int x=0; x<newarr.length; x++){
+        if(x==0){
+          newarr[x]=arr[arr.length-1];
+        }
+        else{
+          newarr[x]=arr[x-1];
+        }
+    }
+    // newarr[0]=arr[arr.length-1];
+    // newarr[1]=arr[0];
+    // newarr[2]=arr[1];
+    // newarr[3]=arr[2];
+    // newarr[4]=arr[3];
+    return newarr;
   }
   
   public static void main(String args[]) {
